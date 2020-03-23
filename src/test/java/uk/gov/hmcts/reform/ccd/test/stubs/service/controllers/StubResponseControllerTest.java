37749723343522acc4d801130886c27a73679f6f
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-03-23 15:31:58 +0000

    RDM-7635: Fixed code coverage issues

--- src/test/java/uk/gov/hmcts/reform/ccd/test/stubs/service/controllers/StubResponseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/ccd/test/stubs/service/controllers/StubResponseControllerTest.java
@@ -2,21 +2,27 @@ package uk.gov.hmcts.reform.ccd.test.stubs.service.controllers;
 
 import com.nimbusds.jose.JOSEException;
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.ccd.test.stubs.service.mock.server.MockHttpServer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class StubResponseControllerTest {
 
@@ -42,7 +48,30 @@ class StubResponseControllerTest {
         doThrow(new IOException("")).when(request).getInputStream();
 
         ResponseEntity<Object> responseEntity = stubResponseController.forwardGetRequests(request);
+        assertNotNull(responseEntity);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @Test
+    @DisplayName("Should return response")
+    void shouldReturnResponse() throws IOException {
+        MockHttpServletRequest request = mock(MockHttpServletRequest.class);
+        when(request.getMethod()).thenReturn("POST");
+        when(request.getCharacterEncoding()).thenReturn("ISO-8859-1");
+        ResponseEntity<Object> responseEntity = new ResponseEntity<>(HttpStatus.OK);
+
+        when(restTemplate.exchange(
+            Matchers.anyString(),
+            Matchers.any(HttpMethod.class),
+            Matchers.any(),
+            Matchers.<Class<Object>> any(),
+            Matchers.<Map<String, String[]>> any()
+            )
+        ).thenReturn(responseEntity);
+
+        ResponseEntity<Object> responseEntityReturned = stubResponseController.forwardPostRequests(request);
+        assertNotNull(responseEntityReturned);
+        assertThat(responseEntityReturned.getStatusCode(), is(HttpStatus.OK));
     }
 
     @Test
@@ -51,6 +80,7 @@ class StubResponseControllerTest {
         HttpServletRequest request = mock(HttpServletRequest.class);
 
         ResponseEntity<Object> responseEntity = stubResponseController.jwkeys(request);
+        assertNotNull(responseEntity);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
     }
 
@@ -60,15 +90,17 @@ class StubResponseControllerTest {
         HttpServletRequest request = mock(HttpServletRequest.class);
 
         ResponseEntity<Object> responseEntity = stubResponseController.openIdToken(request);
+        assertNotNull(responseEntity);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
     }
 
     @Test
-    @DisplayName("Should return openid token")
+    @DisplayName("Should return oauth2 token")
     void shouldReturnOauth2Token() throws JOSEException {
         HttpServletRequest request = mock(HttpServletRequest.class);
 
         ResponseEntity<Object> responseEntity = stubResponseController.oauth2Token(request);
+        assertNotNull(responseEntity);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
     }
 }
