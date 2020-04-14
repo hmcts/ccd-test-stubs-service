98f9186ee42afaa13fcf8f3b0a058c903b196ab9
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-04-14 13:35:04 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,33 +6,10 @@ variable "component" {
   type = "string"
 }
 
-variable "location_app" {
-  type    = "string"
-  default = "UK South"
-}
-
 variable "env" {
   type = "string"
 }
 
-variable "ilbIp" {}
-
 variable "subscription" {}
 
-variable "capacity" {
-  default = "1"
-}
 
-variable "common_tags" {
-  type = "map"
-}
-
-variable "wiremock_server_mappings_path" {
-  description = "WireMock server mappings files path containing json response stubs"
-  type = "string"
-  default = "wiremock"
-}
-
-variable "enable_ase" {
-  default = false
-}
