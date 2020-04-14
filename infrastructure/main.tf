61c37bbf58bfaa35e0a0ed0a4c853540790a3265
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-04-14 13:30:53 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,21 +1,4 @@
-provider "azurerm" {}
-
-locals {
-  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+provider "azurerm" {
+  version = "1.22.1"
 }
 
-module "ccd-testing-support" {
-  source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product             = "${var.product}-${var.component}"
-  location            = "${var.location_app}"
-  env                 = "${var.env}"
-  ilbIp               = "${var.ilbIp}"
-  subscription        = "${var.subscription}"
-  capacity            = "${var.capacity}"
-  common_tags         = "${var.common_tags}"
-  enable_ase          = "${var.enable_ase}"
-
-  app_settings = {
-    WIREMOCK_SERVER_MAPPINGS_PATH = "${var.wiremock_server_mappings_path}"
-  }
-}
