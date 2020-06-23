9d20ea34476a9d4cdb11de9f0367651d4cddfeb5
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2020-06-23 14:51:38 +0100

    removed unused imports

--- src/main/java/uk/gov/hmcts/reform/ccd/test/stubs/service/config/ApplicationConfig.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/test/stubs/service/config/ApplicationConfig.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.ccd.test.stubs.service.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
