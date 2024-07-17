7b9b3f577e3817970ae04694025a26be7755ce7d
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2024-07-17 11:08:49 +0100

    Fix suppressions by upgrading wiremock and json-smart

--- src/main/java/uk/gov/hmcts/reform/ccd/test/stubs/service/config/WireMockServerConfig.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/test/stubs/service/config/WireMockServerConfig.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.ccd.test.stubs.service.config;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,23 +50,22 @@ public class WireMockServerConfig {
         var extension4 = WIREMOCK_EXTENSION_PREFIX + ".DynamicTTLNullResponseTransformer";
         var extension5 = WIREMOCK_EXTENSION_PREFIX + ".DynamicTTLRemoveResponseTransformer";
         var extension6 = WIREMOCK_EXTENSION_PREFIX + ".DynamicTTLUppercaseResponseTransformer";
-        var responseTemplating = new ResponseTemplateTransformer(true);
 
         if (mappingDirectory.isDirectory()) {
             return options()
                 .dynamicHttpsPort()
                 .dynamicPort()
                 .usingFilesUnderDirectory(mappingsPath)
-                .extensions(extension1, extension2, extension3, extension4, extension5, extension6)
-                .extensions(responseTemplating);
+                .templatingEnabled(true)
+                .extensions(extension1, extension2, extension3, extension4, extension5, extension6);
         } else {
             LOG.info("using classpath resources to resolve mappings");
             return options()
                 .dynamicHttpsPort()
                 .dynamicPort()
                 .usingFilesUnderClasspath(mappingsPath)
-                .extensions(extension1, extension2, extension3, extension4, extension5, extension6)
-                .extensions(responseTemplating);
+                .templatingEnabled(true)
+                .extensions(extension1, extension2, extension3, extension4, extension5, extension6);
         }
     }
 }
