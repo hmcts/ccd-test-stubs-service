4e8cb1ee2f485926e1727c5b620c5c4b95551f58
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2025-08-12 18:21:25 +0100

    latest versions

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/test/stubs/service/controllers/StubResponseControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/test/stubs/service/controllers/StubResponseControllerTest.java
@@ -2,9 +2,17 @@ package uk.gov.hmcts.reform.ccd.test.stubs.service.controllers;
 
 import java.util.List;
 
+import org.assertj.core.util.Lists;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.not;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,17 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
 
-@Tag("integration")
 @SpringBootTest
 @AutoConfigureMockMvc
 class StubResponseControllerTest {
